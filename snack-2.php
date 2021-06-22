08c65db76a908d8c006f253b7de345581b88d92f
MAD71241 <michele.deliso@gmail.com>
2021-06-22 16:10:47 +0200

    completo snack 2

---
+++ snack-2.php
@@ -0,0 +1,54 @@
+<?php
+/* Snack 2
+Passare come parametri GET name, mail e age 
+verificare (cercando i metodi che non conosciamo nella documentazione) che:
+- name sia più lungo di 3 caratteri
+- mail contenga un punto e una chiocciola 
+- age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito” 
+altrimenti “Accesso negato” */
+
+$nameLength = strlen($_GET["name"]);
+$checkerDot = strpos($_GET["email"], ".");
+$checkerAt = strpos($_GET["email"], "@");
+$thisAge = intval($_GET["age"]);
+$is_access = "";
+
+if (strlen($_GET["name"]) > 3 && !$checkerDot === false && !$checkerAt === false && is_int($thisAge)) {
+    $is_access = "access granted";
+} else if ($is_access == "") {
+    $is_access = "";
+} else {
+    $is_access = "access denied";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="get">
+
+        <input type="email" name="email" id="" placeholder="type your email here" >
+        <br>
+        <input type="text" name="name" id=""placeholder="type your name here">
+        <br>
+        <input type="number" name="age" id=""placeholder="type your age here">
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
+
+    <h2> <?php echo $is_access ?> </h2>
+</body>
+
+</html>
